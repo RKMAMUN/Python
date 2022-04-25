20789ee1259981dd3180720502eae57acc035ea1
Rezaul Karim Mamun <43470875+RKMAMUN@users.noreply.github.com>
2022-04-25 11:42:52 +0200

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-fact = 1
-num1 = int(input("Enter a Number: "))
-
-if(num1<=0):
-    print("Error")
-else:
-    for i in range(1,(num1+1)):
-        fact = fact*i
-    print("The factorial of",num1,"is: ",fact)
